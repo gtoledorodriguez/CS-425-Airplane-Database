b9a8cf2b23c94a4aa2e999b1dcc20592d8c60095
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-09 20:55:39 -0600

    divided tables needed to be created

--- Airplane_Database.sql
+++ Airplane_Database.sql
@@ -1,1 +1,24 @@
 /* Create Database Tables */
+/*Omesh*/
+/*Customer*/
+
+/*CustAddress*/
+
+/*CardInfo*/
+
+/*Airport*/
+
+
+/*Gladys*/
+/*Flight*/
+
+/*Price*/
+
+/*Nasna*/
+/*Airline*/
+
+/*Booking*/
+
+/*BookedFlights*/
+
+/*MilageProgram*/
