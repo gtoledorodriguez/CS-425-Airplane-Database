8af506d7d2680d5edceb796201a9446a11852206
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-11 18:36:34 -0600

    created airline table, added foreign keys

--- Airplane_Database.sql
+++ Airplane_Database.sql
@@ -10,6 +10,13 @@
 
 
 /*Gladys*/
+/*Airline*/
+CREATE TABLE Airline(
+  airline_id CHAR(2),
+  name varchar(256),
+  country varchar(256),
+  PRIMARY KEY (airline_id)
+);
 /*Flight*/
 CREATE TABLE Flight (
   airline_id CHAR(2),
@@ -21,7 +28,8 @@ CREATE TABLE Flight (
   arrival_time TIME(0) NOT NULL,
   num_ec_seats INT NOT NULL,
   num_fc_seats INT NOT NULL,
-  PRIMARY KEY (airline_id, flight_num, f_date)
+  PRIMARY KEY (airline_id, flight_num, f_date),
+  FOREIGN KEY (airline_id) REFERENCES Airline(airline_id)
 );
 /*Price*/
 CREATE TABLE Price (
@@ -31,12 +39,12 @@ CREATE TABLE Price (
   ec_price INT NOT NULL,
   fc_price INT NOT NULL,
   CHECK (fc_price>ec_price),
-  PRIMARY KEY (airline_id, flight_num, f_date,ec_price,fc_price)
+  PRIMARY KEY (airline_id, flight_num, f_date,ec_price,fc_price),
+  FOREIGN KEY (airline_id, flight_num, f_date) REFERENCES Flight(airline_id, flight_num, f_date)
+
 );
 
 /*Nasna*/
-/*Airline*/
-
 /*Booking*/
 
 /*BookedFlights*/
