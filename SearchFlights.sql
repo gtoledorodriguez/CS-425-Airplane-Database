d44e0fa2c57f84d3a9e376075ef1310858b3c11b
gtoledorodriguez <54423553+gtoledorodriguez@users.noreply.github.com>
2019-11-18 19:36:14 -0600

    Create SearchFlights.sql

---
+++ SearchFlights.sql
@@ -0,0 +1,28 @@
+/*Only specified start and destinations airports should be shown*/
+/*Connection Information:*/
+/*
+depart_airport
+dest_airport
+f_date
+total Price
+duration
+depart_time
+arrival_time
+ec_price if there are open ec spots
+fc_price if there are open fc spots
+*/
+
+/*Choose Return Flight*/
+
+/*limit number of connections*/
+/*  Example */
+/*
+SELECT column_name(s)
+FROM table_name
+WHERE condition
+LIMIT number;
+*/
+
+/*Max length of trip*/
+
+/*price*/
